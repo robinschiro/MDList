d4ab62f915e739f7e6e7241108b05db4ee24d4fc
Ted Kursevicius <kurseviciust@gmail.com>
2017-04-07 14:10:10 -0400

    Added reference marking methods and MDList constructor.

--- src/LockFreeDataStructures/MDList.java
+++ src/LockFreeDataStructures/MDList.java
@@ -5,9 +5,9 @@ package LockFreeDataStructures;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
-public class MDList
+public class MDList<T>
 {
-
+	
     class Node<T>
     {
         private int key;
@@ -23,7 +23,7 @@ public class MDList
             this.children = new ArrayList<>(dimensions);
 
             // Generate mapped key
-
+            
         }
     }
 
@@ -41,11 +41,46 @@ public class MDList
         }
     }
 
+    private static final int Fadp = 0x1;
+	private static final int Fdel = 0x2;
+	private static final int Fall = 0x3;
+	
     private int dimensions;
     private int keySpace;
 
     // Set upon construction
     private int base;
 
-
+    public MDList( int dimensions, int keySpace )
+    {
+    	this.dimensions = dimensions;
+    	this.keySpace = keySpace;
+    	this.base = (int) Math.pow(keySpace, 1/dimensions);
+    }
+    
+    
+    
+    void SetMark( AtomicStampedReference<Node<T>> node, int mark )
+    {
+    	int[] stampHolder = {0};
+    	Node<T> pointer = node.get(stampHolder);
+    	stampHolder[0] = stampHolder[0] | mark;
+    	node.attemptStamp(pointer, stampHolder[0]);
+    }
+    
+    void ClearMark( AtomicStampedReference<Node<T>> node, int mark)
+    {
+    	int[] stampHolder = {0};
+    	Node<T> pointer = node.get(stampHolder);
+    	stampHolder[0] = stampHolder[0] & ~mark;
+    	node.attemptStamp(pointer, stampHolder[0]);
+    }
+    
+    boolean IsMarked( AtomicStampedReference<Node<T>> node, int mark)
+    {
+    	int stamp = node.getStamp();
+    	stamp = 0x3 & stamp;
+    	stamp = stamp & mark;
+    	return (stamp == mark);
+    }
 }
