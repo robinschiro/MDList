382202dc69576acc50f253ab12f10c8cbfc722b9
Robin Schiro <robinschiro@gmail.com>
2017-04-08 16:30:00 -0400

    Fixed formatting, created Eclipse format config.
    Signed-off-by: Robin Schiro <robinschiro@gmail.com>

--- src/LockFreeDataStructures/MDList.java
+++ src/LockFreeDataStructures/MDList.java
@@ -7,7 +7,6 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class MDList<T>
 {
-
     class Node<V>
     {
         private int key;
@@ -42,8 +41,8 @@ public class MDList<T>
     }
 
     private static final int Fadp = 0x1;
-	private static final int Fdel = 0x2;
-	private static final int Fall = 0x3;
+    private static final int Fdel = 0x2;
+    private static final int Fall = 0x3;
 
     private int dimensions;
     private int keySpace;
@@ -57,45 +56,45 @@ public class MDList<T>
 
     public MDList( int dimensions, int keySpace )
     {
-    	this.dimensions = dimensions;
-    	this.keySpace = keySpace;
-    	this.base = (int) Math.pow(keySpace, 1/dimensions);
-    	this.head = new AtomicStampedReference<>(new Node(0, null), 0);
+        this.dimensions = dimensions;
+        this.keySpace = keySpace;
+        this.base = (int) Math.pow(keySpace, 1 / dimensions);
+        this.head = new AtomicStampedReference<>(new Node(0, null), 0);
     }
 
-    private boolean SetMark(AtomicStampedReference<Node<T>> node, int mark )
+    private boolean SetMark( AtomicStampedReference<Node<T>> node, int mark )
     {
-    	int[] stampHolder = {0};
-    	Node<T> pointer = node.get(stampHolder);
-    	stampHolder[0] = stampHolder[0] | mark;
-    	return node.attemptStamp(pointer, stampHolder[0]);
+        int[] stampHolder = { 0 };
+        Node<T> pointer = node.get(stampHolder);
+        stampHolder[0] = stampHolder[0] | mark;
+        return node.attemptStamp(pointer, stampHolder[0]);
     }
 
-    private boolean ClearMark(AtomicStampedReference<Node<T>> node, int mark)
+    private boolean ClearMark( AtomicStampedReference<Node<T>> node, int mark )
     {
-    	int[] stampHolder = {0};
-    	Node<T> pointer = node.get(stampHolder);
-    	stampHolder[0] = stampHolder[0] & ~mark;
-    	return node.attemptStamp(pointer, stampHolder[0]);
+        int[] stampHolder = { 0 };
+        Node<T> pointer = node.get(stampHolder);
+        stampHolder[0] = stampHolder[0] & ~mark;
+        return node.attemptStamp(pointer, stampHolder[0]);
     }
 
-    private boolean IsMarked(AtomicStampedReference<Node<T>> node, int mark)
+    private boolean IsMarked( AtomicStampedReference<Node<T>> node, int mark )
     {
-    	int stamp = node.getStamp();
-    	stamp = 0x3 & stamp;
-    	stamp = stamp & mark;
-    	return (stamp == mark);
+        int stamp = node.getStamp();
+        stamp = 0x3 & stamp;
+        stamp = stamp & mark;
+        return ( stamp == mark );
     }
 
-    private int[] KeyToCoord(int key)
+    private int[] KeyToCoord( int key )
     {
-    	int partialKey = key;
+        int partialKey = key;
         int[] mappedKey = new int[base];
 
-        for( int dim = dimensions - 1; partialKey > 0; dim-- )
+        for ( int dim = dimensions - 1; partialKey > 0; dim-- )
         {
-        	mappedKey[dim] = partialKey % base;
-        	partialKey = partialKey/base;
+            mappedKey[dim] = partialKey % base;
+            partialKey = partialKey / base;
         }
 
         return mappedKey;
@@ -104,48 +103,48 @@ public class MDList<T>
     private T Find( int key )
     {
         AtomicStampedReference<Node<T>> curr, pred;
-    	int[] dimOfPred = {0};
-    	int[] dimOfCurr = {0};
+        int[] dimOfPred = { 0 };
+        int[] dimOfCurr = { 0 };
 
-    	pred = null;
-    	curr = head;
+        pred = null;
+        curr = head;
 
-    	LocatePred( KeyToCoord(key), curr, pred, dimOfPred, dimOfCurr );
+        LocatePred(KeyToCoord(key), curr, pred, dimOfPred, dimOfCurr);
 
-    	if( dimOfCurr[0] == dimensions )
-    	{
-    		return curr.getReference().value;
-    	}
-    	return null;
+        if ( dimOfCurr[0] == dimensions )
+        {
+            return curr.getReference().value;
+        }
+        return null;
     }
 
     private void LocatePred( int[] mappedKey, AtomicStampedReference<Node<T>> curr, AtomicStampedReference<Node<T>> pred, int[] dimOfPred, int[] dimOfCurr )
     {
-    	while( dimOfCurr[0] < dimensions )
-    	{
-    		while( curr != null && mappedKey[dimOfCurr[0]] > curr.getReference().mappedKey[dimOfCurr[0]] )
-    		{
-    			pred = curr;
-    			dimOfPred[0] = dimOfCurr[0];
-    			AdoptionDescriptor<T> adesc = curr.getReference().adoptDesc;
+        while ( dimOfCurr[0] < dimensions )
+        {
+            while ( curr != null && mappedKey[dimOfCurr[0]] > curr.getReference().mappedKey[dimOfCurr[0]] )
+            {
+                pred = curr;
+                dimOfPred[0] = dimOfCurr[0];
+                AdoptionDescriptor<T> adesc = curr.getReference().adoptDesc;
 
-    			if( adesc != null && dimOfPred[0] >= adesc.dimOfPred && dimOfPred[0] <= adesc.dimOfCurr )
-    			{
-    				//FinishInserting(curr, adesc);
-    			}
-    			// paper has this as curr = ClearMark(curr.child[dc], Fall)
-    			// does this mean that clear mark should return the node?
-    			ClearMark( curr.getReference().children.get(dimOfCurr[0]), Fall );
-    		}
-    		if( curr == null || mappedKey[dimOfCurr[0]] < curr.getReference().mappedKey[dimOfCurr[0]] )
-    		{
-    			break;
-    		}
-    		else
-    		{
-    			dimOfCurr[0] = dimOfCurr[0] + 1;
-    		}
-    	}
+                if ( adesc != null && dimOfPred[0] >= adesc.dimOfPred && dimOfPred[0] <= adesc.dimOfCurr )
+                {
+                    // FinishInserting(curr, adesc);
+                }
+                // paper has this as curr = ClearMark(curr.child[dc], Fall)
+                // does this mean that clear mark should return the node?
+                ClearMark(curr.getReference().children.get(dimOfCurr[0]), Fall);
+            }
+            if ( curr == null || mappedKey[dimOfCurr[0]] < curr.getReference().mappedKey[dimOfCurr[0]] )
+            {
+                break;
+            }
+            else
+            {
+                dimOfCurr[0] = dimOfCurr[0] + 1;
+            }
+        }
     }
 
 }
